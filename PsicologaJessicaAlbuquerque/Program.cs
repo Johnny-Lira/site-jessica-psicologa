343b0c208430ad6a609fd867554ba5414b7c79e8
Johnny-Lira <johnnyjlira@gmail.com>
2025-09-12 23:53:32 -0300

    Ajustes layout e funcionalidade de mostrar/esconder menu e tema escuro

---
+++ PsicologaJessicaAlbuquerque/Program.cs
@@ -0,0 +1,30 @@
+using MudBlazor.Services;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddMudServices();
+
+WebApplication app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+await app.RunAsync();
